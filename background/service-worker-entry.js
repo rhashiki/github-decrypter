a7441f686ccb3a5f66356e2d2bdbec77de3cb9e0
rhashiki <ivancdongoncalves@gmail.com>
2026-08-26 18:01:01 -0300

    feat: install automatic suggestions runtime

--- background/service-worker-entry.js
+++ background/service-worker-entry.js
@@ -1,6 +1,8 @@
 import { installGuardedCommit } from '../core/guarded-commit.js';
 import { installCheckpointRuntime } from './checkpoint-runtime.js';
+import { installSuggestionsRuntime } from './suggestions-runtime.js';
 
 installGuardedCommit();
 installCheckpointRuntime();
+installSuggestionsRuntime();
 await import('./service-worker.js');
