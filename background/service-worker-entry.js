5f4ac139c86fcdd3b9b8a7d48ac45eaa6d9c8990
rhashiki <ivancdongoncalves@gmail.com>
2026-08-26 17:42:52 -0300

    feat: install authoritative checkpoint runtime

--- background/service-worker-entry.js
+++ background/service-worker-entry.js
@@ -1,4 +1,6 @@
 import { installGuardedCommit } from '../core/guarded-commit.js';
+import { installCheckpointRuntime } from './checkpoint-runtime.js';
 
 installGuardedCommit();
+installCheckpointRuntime();
 await import('./service-worker.js');
