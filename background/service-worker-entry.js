233a2a3f662bae7f83d136400d8a29bb2fe57f8d
rhashiki <ivancdongoncalves@gmail.com>
2026-08-26 21:49:38 -0300

    feat: bootstrap GitHub App runtime

--- background/service-worker-entry.js
+++ background/service-worker-entry.js
@@ -1,7 +1,9 @@
 import './guarded-commit-bootstrap.js';
 import { installCheckpointRuntime } from './checkpoint-runtime.js';
 import { installSuggestionsRuntime } from './suggestions-runtime.js';
+import { installGithubAppRuntime } from './github-app-runtime.js';
 import './service-worker.js';
 
 installCheckpointRuntime();
 installSuggestionsRuntime();
+installGithubAppRuntime();
