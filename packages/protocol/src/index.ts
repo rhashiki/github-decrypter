87fea21a4e07437a19767a2edea110539dca8d44
rhashiki <ivancdongoncalves@gmail.com>
2026-09-03 07:16:18 -0300

    build 7: expose shared protocol surface

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,1 +1,11 @@
 export const packageIdentity = '@github-decrypter/protocol' as const;
+
+export * from './json.js';
+export * from './version.js';
+export * from './ids.js';
+export * from './peer.js';
+export * from './errors.js';
+export * from './envelope.js';
+export * from './messages.js';
+export * from './handshake.js';
+export * from './guards.js';
