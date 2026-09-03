b1ac282003585bb5de74f180e4c3291eff681284
rhashiki <ivancdongoncalves@gmail.com>
2026-09-03 08:34:50 -0300

    build 10: promote local runtime app from placeholder to daemon authority

--- apps/local/src/index.ts
+++ apps/local/src/index.ts
@@ -1,11 +1,6 @@
-import { PROTOCOL_SCHEMA, type PeerRole } from '@github-decrypter/protocol';
-
-const protocolRole: PeerRole = 'local-runtime';
-
-export const appIdentity = Object.freeze({
-  id: 'local',
-  packageName: '@github-decrypter/local',
-  protocolRole,
-  protocolSchema: PROTOCOL_SCHEMA,
-  role: 'Durable local runtime process placeholder; daemon implementation begins in Build 10.'
-});
+export * from './config.js';
+export * from './daemon.js';
+export * from './identity.js';
+export * from './instance-lock.js';
+export * from './lifecycle.js';
+export * from './server.js';
